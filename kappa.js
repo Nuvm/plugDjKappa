ad2a87401f424445ead3f3c3e6aeed8a0399278e
Nuvm <nuvmyt@gmail.com>
2015-05-25 18:49:57 -0400

    Update kappa.js

--- kappa.js
+++ kappa.js
@@ -1,5 +1,8 @@
-$('body').append('<div type="button" onclick="rmv()" style="position:absolute; top:90px; right: 355px; height: 200px; width: 250px; background-color: black; border: 2.5px solid white; border-radius: 20px; opacity: .8; z-index: 200;">Ultimate Kappa Button</div>');
+$('body').append('<div id="kappaButton" type="button" onclick="rmv()" style="position:absolute; top:90px; right: 355px; height: 200px; width: 250px; background-color: black; border: 2.5px solid white; border-radius: 20px; opacity: .8; z-index: 200;">Ultimate Kappa Button</div>');
+$('#kappaButton').hover(function(){$('#kappaButton').css("cursor","pointer");},function(){$('#kappaButton').css("cursor","auto");});
 function rmv(){
-  $('body').remove();
-  $('body').append('<div style="font-size=5000%; height: 500px; width: 1000px; background-color: black; opacity: .8;">KAPPA ULTED</div>');
+  $('#kappaButton').text("THE KAPPA WILL ULT IN 5 SECONDS!!!!" + <br> + "PREPARE, FUCKING PLEB");
+  setTimeout(function(){$('#kappaButton').text("jk the kappa will just delete this page");},5100);
+  setTimeout(function(){$('#kappaButton').text("HERE WE GO FUCKERS");},7100);
+  setTimeout(function(){$('body').remove();},8000);
 }
