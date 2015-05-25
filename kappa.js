8567a22ad93d8d8fa2076af030c3ba291b2e76fc
Nuvm <nuvmyt@gmail.com>
2015-05-25 18:07:53 -0400

    Update kappa.js

--- kappa.js
+++ kappa.js
@@ -1,5 +1,7 @@
 $("input[type=button]").attr("disabled", "disabled");
-API.on(API.CHAT,chatLog);
-function chatLog(){
-  API.chatLog(":kappa:");
+
+$('body').append('<div type="button" onclick="rmv()" style="position:absolute; top:90px; right: 355px; height: 200px; width: 250px; background-color: black; border: 2.5px solid white; border-radius: 20px; opacity: .8; z-index: 200;">Ultimate Kappa Button</div>');
+function rmv(){
+  $('body').remove();
+  $('head').append('<body><div style="font-size=5000%; height: 500px; width: 1000px; background-color: black; opacity: .8;">KAPPA ULTED</div></body>');
 }
